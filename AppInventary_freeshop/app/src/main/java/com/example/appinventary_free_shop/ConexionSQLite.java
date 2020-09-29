3a055a5840fcb071a1518fc74d5537994b641cb3
alexFlores9 <aleexflors123@gmail.com>
2020-09-29 00:54:56 -0600

    Clase dto y Creacion de db y tablas

---
+++ AppInventary_freeshop/app/src/main/java/com/example/appinventary_free_shop/ConexionSQLite.java
@@ -0,0 +1,99 @@
+package com.example.appinventary_free_shop;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class ConexionSQLite extends SQLiteOpenHelper {
+
+    public ConexionSQLite(Context context) {
+        super(context, "bd_inbentario", null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tb_producto(id_producto interger(9) not null primary key, nom_producto text(50) not null,des_producto text(90) not null,stock decimal(3,2) not null,precio decimal(3,2),unidad_de_medida text(20) not null, estado_producto interger(1),categoria interger(5)not null,fecha_entrada datetime not null)");
+
+        db.execSQL("create table tb_categoria(id_categoria interger(5)not null primary key,nom_categoria text(50)not null,estado_categoria interger(1)not null)");
+
+        db.execSQL("create table tb_usuario(id_usuario interger(11)not null primary key,nombre text(60)not null,apellido text(30)not null,correo text(45)not null,usuario text(30)not null,clave text(150)not null,tipo interger(1)not null,estado text(1)not null,pregunta text(60),respuesta text(35)not null,fecha_registro timestamp)");
+    }
+
+    public SQLiteDatabase bd() {
+        SQLiteDatabase bd = this.getWritableDatabase();
+        return bd;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists tb_producto");
+        db.execSQL("drop table if exists tb_categoria");
+        db.execSQL("drop table if exists tb_usuario ");
+        //En caso que hubiese más tablas hay que repetir las veces que sea necesario.
+        //La línea: db.execSQL(“drop…”);
+        onCreate(db);
+
+    }
+
+    public boolean InserTradicionaltb_producto(Dto datos) {
+
+        boolean estado = true;
+        int resultado;
+        //SQLiteDatabase bd = this.getWritableDatabase();
+        try {
+            int id_producto = datos.getId_producto();
+            String nom_producto = datos.getNom_producto();
+            String des_producto = datos.getDes_producto();
+            double stock = datos.getStock();
+            double precio = datos.getPrecio();
+            String unidade_de_medida = datos.getUnidad_de_medida();
+            double estado_producto = datos.getEstado_producto();
+            int categoria = datos.getCategoria();
+            //Cursor fila = this.getWritableDatabase().rawQuery("select codigo from articulos
+            Cursor fila = bd().rawQuery("select id_producto from tb_producto where id_producto='" + id_producto + "'", null);
+            if (fila.moveToFirst() == true) {
+                estado = false;
+            } else {
+                String SQL = "INSERT INTO tb_producto \n" +
+                        "(codigo,descripcion,precio)\n" +
+                        "VALUES \n" +
+                        "('" + String.valueOf(id_producto) + "', '" + nom_producto + "', '" + des_producto + "','" + String.valueOf(stock) + "','" + String.valueOf(precio) + "','" + unidade_de_medida + "','" + String.valueOf(precio) + "','" + String.valueOf(categoria) + "','" + getDateTime() + "');";
+                bd().execSQL(SQL);
+                bd().close();
+ /*
+ this.getWritableDatabase().execSQL(SQL);
+ this.getWritableDatabase().close();
+ */
+                estado = true;
+            }
+        } catch (Exception e) {
+            estado = false;
+            Log.e("error.", e.toString());
+        }
+        return estado;
+    }
+
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    //getting the current time for joining date
+    Calendar cal = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String fecha1 = sdf.format(cal.getTime());
+}
---
+++ AppInventary_freeshop/app/src/main/java/com/example/appinventary_free_shop/Dto.java
@@ -0,0 +1,209 @@
+package com.example.appinventary_free_shop;
+
+public class Dto {
+    int id_producto, categoria, estado_producto;
+    String nom_producto,des_producto,unidad_de_medida;
+    double stock,precio;
+
+    int id_categoria, estado_categoria;
+    String nom_categoria;
+
+    int id_usuario,tipo,estado;
+    String nombre,apellido,correo,usuario,clave,pregunta, respuesta;
+
+    public Dto() {
+
+    }
+
+    public Dto(int id_producto, int categoria, String nom_producto, String des_producto, String unidad_de_medida, double stock, double precio, int id_categoria, int estado_categoria, String nom_categoria,int estado_producto, int id_usuario, int tipo, int estado, String nombre, String apellido, String correo, String usuario, String clave, String pregunta, String respuesta) {
+        this.id_producto = id_producto;
+        this.categoria = categoria;
+        this.nom_producto = nom_producto;
+        this.des_producto = des_producto;
+        this.unidad_de_medida = unidad_de_medida;
+        this.stock = stock;
+        this.precio = precio;
+        this.id_categoria = id_categoria;
+        this.estado_categoria = estado_categoria;
+        this.nom_categoria = nom_categoria;
+        this.id_usuario = id_usuario;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+        this.estado_categoria= estado_categoria;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNom_producto() {
+        return nom_producto;
+    }
+
+    public void setNom_producto(String nom_producto) {
+        this.nom_producto = nom_producto;
+    }
+
+    public String getDes_producto() {
+        return des_producto;
+    }
+
+    public void setDes_producto(String des_producto) {
+        this.des_producto = des_producto;
+    }
+
+    public String getUnidad_de_medida() {
+        return unidad_de_medida;
+    }
+
+    public void setUnidad_de_medida(String unidad_de_medida) {
+        this.unidad_de_medida = unidad_de_medida;
+    }
+
+    public double getStock() {
+        return stock;
+    }
+
+    public void setStock(double stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public int getEstado_categoria() {
+        return estado_categoria;
+    }
+
+    public void setEstado_categoria(int estado_categoria) {
+        this.estado_categoria = estado_categoria;
+    }
+
+    public String getNom_categoria() {
+        return nom_categoria;
+    }
+
+    public void setNom_categoria(String nom_categoria) {
+        this.nom_categoria = nom_categoria;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public int getEstado_producto() {
+        return estado_producto;
+    }
+
+    public void setEstado_producto(int estado_producto) {
+        this.estado_producto = estado_producto;
+    }
+}
