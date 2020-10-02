6007e16249f201146116a8a3f4b426de757fbea4
oscarnoelopezceron <61834003+oscarnoelopezceron@users.noreply.github.com>
2020-10-02 17:24:42 -0600

    tb_categoria vol 1
    slash y tb_categoria. solo creación de la actividad y modificación del slash

---
+++ AppInventary_freeshop/app/src/main/java/com/example/appinventary_free_shop/tb_categoria.java
@@ -0,0 +1,14 @@
+package com.example.appinventary_free_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tb_categoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tb_categoria);
+    }
+}
